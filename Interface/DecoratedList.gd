7c56afdacf019b218902125a2001630f08146625
NancokPS2 <nancok@tuta.io>
2023-06-07 08:59:29 -0300

    DecoratedList finished

--- Interface/DecoratedList.gd
+++ Interface/DecoratedList.gd
@@ -2,26 +2,27 @@ extends VBoxContainer
 class_name DecoratedList
 
 
-@export var entries:Array[NodePath]
+@export var entries:Array[StringName]
 @export var icons:Array[Texture] #entryIndex:Texture
 @export var defaultObject:Node
 
-#func _ready():
+func _ready():
+	update_from_entries()
 	
+func set_fonts(nameFont:Font, valFont:Font):
+	for child in get_children():
+		if child is Entry:
+			child.nameFont = nameFont
+			child.valFont = valFont
+			child.update_entry()
 
-#func get_properties()->Array:
-#	var props:Array
-#	for entry in entries:
-#		defaultObject.get_indexed(entry)
-#		props.append(1)
-#	return props
 func update_from_entries(objectUsed:Object = defaultObject):
 	for child in get_children(): child.queue_free()
 	
 	var index:int=0
 	for entry in entries:
 		var icon:Texture
-		if icons.size() < index: 
+		if not icons.is_empty() and index < icons.size(): 
 			icon = icons[index]
 		else:
 			icon = null
@@ -30,11 +31,11 @@ func update_from_entries(objectUsed:Object = defaultObject):
 		index+=1
 
 	
-func add_entry_node(object:Object, entry:NodePath, displayedName:String = "", icon:Texture = Texture.new()):
+func add_entry_node(object:Object, entry:StringName, displayedName:String = "", icon:Texture = Texture.new()):
 	
 	var entryName:String 
 	if displayedName == "":
-		entryName = entry.get_subname(entry.get_subname_count()-1)
+		entryName = entry.split(":")[-1]
 	else:
 		entryName = displayedName
 	var entryObj:=Entry.new(object, entry, entryName, icon)
@@ -47,10 +48,13 @@ func add_entry_node(object:Object, entry:NodePath, displayedName:String = "", ic
 
 
 
-class Entry extends Control:
+class Entry extends HBoxContainer:
+	
+	var nameFont:Font
+	var valFont:Font
 	
 	var objectTracked:Object
-	var propertyPath:NodePath
+	var propertyPath:StringName
 	var iconTexture:Texture
 	
 	var iconNode:=TextureRect.new()
@@ -60,13 +64,23 @@ class Entry extends Control:
 	
 	var entryName:String
 	
-	func _init(_objectTracked:Object, _propertyPath:NodePath, _entryName:String, _iconTexture:Texture) -> void:
+	func _init(_objectTracked:Object, _propertyPath:StringName, _entryName:String, _iconTexture:Texture) -> void:
 		objectTracked = _objectTracked 
 		propertyPath = _propertyPath
 		entryName = _entryName
 		iconTexture = _iconTexture
 	
+	func _ready() -> void:
+		add_child(iconNode)
+		add_child(nameLabel)
+		add_child(valLabel)
+		update_entry()
 	
 	func update_entry():
 		nameLabel.text = entryName
-		valLabel.text = str(objectTracked.get_indexed(propertyPath))
+		valLabel.text = str(objectTracked.get_indexed(NodePath(propertyPath)))
+		iconNode.texture = iconTexture
+		if nameFont is Font: nameLabel.add_theme_font_override("font",nameFont)
+		if valFont is Font: valLabel.add_theme_font_override("font",valFont)
+		
+		
