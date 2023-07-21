37b132551424388a1f1c543965cdf7be0999c251
NancokPS2 <nancok@tuta.io>
2023-07-21 18:31:19 -0300

    Several fixes

--- SceneSpawner.gd
+++ SceneSpawner.gd
@@ -42,7 +42,7 @@ signal spawn_prevented_by_limit #Emmited if a spawn was attempted but the limit 
 var _spawnOrderCounter:int = 0:
 	set(val):
 		if val < 0: push_error("This is an index so it cannot be negative.")
-		elif val >= spawnOrder.size(): push_error( "Index out of bounds. spawnOrder size is {0} but the value was {1}".format([spawnOrder.size(), val]) )
+		elif val >= spawnOrder.size(): _spawnOrderCounter = 0 #push_error( "Index out of bounds. spawnOrder size is {0} but the value was {1}".format([spawnOrder.size(), val]) )
 		else: _spawnOrderCounter = val
 
 ## Scenes that will be spawned.
@@ -50,7 +50,6 @@ var _spawnOrderCounter:int = 0:
 
 ## Initial positions of the scenes.
 @export var spawnLocations:Array[Vector3] = [Vector3.ZERO]
-
 
 var spawnTimer:Timer=Timer.new()
 var spawnedSceneRefs:Array[Node]
@@ -78,25 +77,25 @@ func spawn_scene():
 	node.position = spawnLocations.pick_random()
 	reference_node(node)
 	spawnParent.add_child(node)
-	emit_signal("spawned_node", node, node.position)
+	spawned_node.emit(node, node.position)
 	
 ## Check to ensure everything is ready to spawn.
 func validate_spawn_order()->bool:
 	if _spawnOrderCounter >= scenesToSpawn.size(): _spawnOrderCounter = 0; push_warning("_spawnOrderCounter was out of bounds, fixed but this is not intended.")
 	
 	var anyOutOfBounds:bool = spawnOrder.any( func(val): return val < scenesToSpawn.size())
-	if anyOutOfBounds: push_error("spawnOrder has out of bounds integers. Switching to random mode."); spawnOrder = []
+	if anyOutOfBounds: push_error("spawnOrder has out of bounds integers. Switching to random mode."); spawnOrder = []; return false
 	else: return true
 	
 ## Attempts to clear any invalid references. Like freed nodes.
 func clean_spawn_refs():
 	if nodesMustRemainInParent:
-	for node in spawnedSceneRefs:
-		if not node or (node and node.get_parent() != spawnParent):
-			unreference_node(node)
+		for node in spawnedSceneRefs:
+			if not node or (node and node.get_parent() != spawnParent):
+				unreference_node(node)
 	else:
 		for node in spawnedSceneRefs:
-			if not ( node and is_valid_instance(node) ): 
+			if not ( node and is_instance_valid(node) ): 
 				unreference_node(node)
 		
 func unreference_node(node:Node):
@@ -108,11 +107,11 @@ func reference_node(node:Node):
 	else: spawnedSceneRefs.append(node)
 	
 func get_next_node()->Node:
+	var node:Node
 	if spawnOrder.is_empty():
-		var node:Node = scenesToSpawn.pick_random().duplicate().instantiate()
+		node = scenesToSpawn.pick_random().duplicate().instantiate()
 	else:
-		if not validate_spawn_order(): return
-		var node:Node = scenesToSpawn[_spawnOrderCounter].instantiate()
+		node = scenesToSpawn[_spawnOrderCounter].instantiate()
 		_spawnOrderCounter+=1
 		if _spawnOrderCounter >= scenesToSpawn.size(): _spawnOrderCounter = 0
 	return node
