9b0dbd484bf93e037b6a11787642e4397274f370
LR <55665720+NancokPS2@users.noreply.github.com>
2023-10-28 01:42:34 -0300

    Update TopDownMovementComponentCharacter2D.gd

--- EC/CharacterBody2D/TopDownMovementComponentCharacter2D.gd
+++ EC/CharacterBody2D/TopDownMovementComponentCharacter2D.gd
@@ -1,14 +1,8 @@
 extends ComponentNode
 class_name CharacterBody2DComponentTopDownMovement
-## A simple top-down movement controller.
+## A simple top-down movement controller. It moves the node by acceleration * currentDirection and applies drag when no direction is given to make the node stop moving.
 
-#@export var inputActions:Dictionary = {
-#	UP = "move_up",
-#	DOWN = "move_down",
-#	LEFT = "move_left",
-#	RIGHT = "move_right"
-#
-#}
+
 @export var acceleration:float = 1000  
 #@export_range(1,5, 0.1, "or_greater") var accelerationCounterBonus:float = 1.
 @export_range(0,1) var dragFactorOnStop:float = 0.8
@@ -27,8 +21,6 @@ func _parent_update():
 
 	
 #New funcs
-#func get_direction()->Vector2:
-#	return Input.get_vector("move_left","move_right","move_up","move_down").normalized()
 func set_currentDirection(dir:Vector2):
 	currentDirection = dir
 
@@ -37,10 +29,6 @@ func set_acceleration(accel:float):
 				
 func _physics_process(delta: float) -> void:
 	var velocityAdded:Vector2 = currentDirection * acceleration * delta
-	
-	#If trying to turn from high speeds, do it faster.
-#	if (velocityAdded + targetNode.velocity).length() < targetNode.velocity.length() and targetNode.velocity.length() > acceleration / 2:
-#		velocityAdded *= accelerationCounterBonus
 	
 	targetNode.velocity += velocityAdded
 	if currentDirection == Vector2.ZERO:
